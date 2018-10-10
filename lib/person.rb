25c4a4ba4240ab1f3c621e7b702e73c488c9753c
Min Hee  Park <minheekangg@gmail.com>
2018-10-10 18:03:11 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,16 @@
 
 class Person
 attr_reader :name
-attr_accessor :bank_account, :happiness
+attr_accessor :bank_account
 
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
+  end
+
+  def happiness=(happiness)
+    
   end
 
   def bank_account
